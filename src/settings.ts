524da964af74f124320979272e18c2c4a2526aa8
Thomas Lebeau <lebeau.thomas@gmail.com>
2022-01-02 18:40:12 +0100

    chore: styling

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'ThermobeaconWs08';
+export const PLATFORM_NAME = "ThermobeaconWs08";
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-thermobeacon-ws08';
+export const PLUGIN_NAME = "homebridge-thermobeacon-ws08";
